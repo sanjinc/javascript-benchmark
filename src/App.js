40e039102a452488418ecd9adf8a6e13d00213a9
Sanjin Celeski <celeski.sanjin@gmail.com>
2022-12-02 07:04:46 +0000

    Update App.js and styles.css

--- src/App.js
+++ src/App.js
@@ -18,15 +18,15 @@ let intervalCount = 0;
 worker1.postMessage({
   config: {
     duration: DURATION,
-    interval: INTERVAL
-  }
+    interval: INTERVAL,
+  },
 });
 
 worker2.postMessage({
   config: {
     duration: DURATION,
-    interval: INTERVAL
-  }
+    interval: INTERVAL,
+  },
 });
 
 worker1.addEventListener("error", function (event) {
@@ -146,88 +146,165 @@ export default function App() {
   };
 
   return (
-    <div className="container">
-      <img src="js.svg" className="js" />
-      <p className="footer">
-        <a href="https://www.linkedin.com/in/sanjinceleski/" target="_blank">
-          Developed with passion 🇦🇪
-        </a>
-      </p>
-      <div className="wrapper">
-        <div className="content">
-          <h1>
-            Benchmark <span className="rocket">🚀</span>
-          </h1>
-          <h2>Compare JavaScript Performance</h2>
-          <div className="benchmarks">
-            <div>
-              {/* <h3>
+    <div>
+      <div class="page">
+        <img src="js.svg" className="js" />
+        <p className="footer">
+          <a href="#sanjin-celeski">Developed with passion ❤️</a>
+        </p>
+        <div className="wrapper">
+          <div className="container">
+            <div className="content">
+              <div className="title-container">
+                <h1 className="title">
+                  Benchmark <span className="rocket">🚀</span>
+                </h1>
+              </div>
+              <h2 class="subtitle">Compare JavaScript Performance</h2>
+              <div className="benchmarks">
+                <div className="benchmark">
+                  {/* <h3>
                 {isBenchmarking
                   ? `${count1.toLocaleString("en-US")} ops`
                   : `${parseInt(count1 / (DURATION / 1000)).toLocaleString(
                       "en-US"
                     )} op/s`}
               </h3> */}
-              <textarea
-                ref={result1Ref}
-                defaultValue="// is this faster?&#13;&#10;var answer = 42;"
-              ></textarea>
-            </div>
-            <div className="run-container">
-              <div className="run" onClick={handleOnClickWorker}>
-                <div className={`gear-icon ${isReady ? "" : "rotate"}`}>
-                  <img src="gear.svg" />
+                  <textarea
+                    ref={result1Ref}
+                    defaultValue="// is this faster?&#13;&#10;[1, 2].concat(4, 8);"
+                  ></textarea>
                 </div>
-                <div className="play-icon-container">
-                  {isReady && <div className="play-icon"></div>}
+                <div className="run-container">
+                  <div
+                    className={`run ${isReady ? "" : "run--busy"}`}
+                    onClick={handleOnClickWorker}
+                  >
+                    <div
+                      className={`gear-icon ${
+                        isReady ? "" : "gear-icon--rotate"
+                      }`}
+                    >
+                      <img src="gear.svg" />
+                    </div>
+                    <div className="play-icon-container">
+                      {isReady && <div className="play-icon"></div>}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div>
-              {/* <h3>
+                <div className="benchmark">
+                  {/* <h3>
                 {isBenchmarking
                   ? `${count2.toLocaleString("en-US")} ops`
                   : `${parseInt(count2 / (DURATION / 1000)).toLocaleString(
                       "en-US"
                     )} op/s`}
               </h3> */}
-              <textarea
-                ref={result2Ref}
-                defaultValue="// or is this faster?&#13;&#10;const answer = 42;"
-              ></textarea>
-            </div>
-          </div>
-          {/* <button onClick={handleOnClickWorker} disabled={!isReady}>
+                  <textarea
+                    ref={result2Ref}
+                    defaultValue="// or this faster?&#13;&#10;[1, 2, ...[4, 8]];"
+                  ></textarea>
+                </div>
+              </div>
+              {/* <button onClick={handleOnClickWorker} disabled={!isReady}>
             {isReady ? "WORK" : "WORKING"}
           </button> */}
-          <div className={`rails ${dirty ? "" : "rails--hidden"}`}>
-            <div className="rail">
-              <div
-                className={`bar ${isBarAnimated ? "bar--animated" : ""}`}
-                style={{
-                  width: `${
-                    count1 && maxCount ? (count1 / maxCount) * 100 : 0
-                  }%`,
-                  transitionDuration: isBarAnimated
-                    ? `${TRANSITIONS_DURATION}s`
-                    : "unset"
-                }}
-              ></div>
-            </div>
-            <div className="rail">
-              <div
-                className={`bar ${isBarAnimated ? "bar--animated" : ""}`}
-                style={{
-                  width: `${
-                    count2 && maxCount ? (count2 / maxCount) * 100 : 0
-                  }%`,
-                  transitionDuration: isBarAnimated
-                    ? `${TRANSITIONS_DURATION}s`
-                    : "unset"
-                }}
-              ></div>
+              <div className={`rails ${dirty ? "" : "rails--hidden"}`}>
+                <div className="rail">
+                  <div
+                    className={`bar ${isBarAnimated ? "bar--animated" : ""}`}
+                    style={{
+                      width: `${
+                        count1 && maxCount ? (count1 / maxCount) * 100 : 0
+                      }%`,
+                      ...(isBarAnimated && {
+                        transitionDuration: `${TRANSITIONS_DURATION}s`,
+                      }),
+                    }}
+                  ></div>
+                </div>
+                <div className="rail">
+                  <div
+                    className={`bar ${isBarAnimated ? "bar--animated" : ""}`}
+                    style={{
+                      width: `${
+                        count2 && maxCount ? (count2 / maxCount) * 100 : 0
+                      }%`,
+                      ...(isBarAnimated && {
+                        transitionDuration: `${TRANSITIONS_DURATION}s`,
+                      }),
+                    }}
+                  ></div>
+                </div>
+              </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="info">
+        <div className="container container--info">
+          <p>Hello 👋</p>
+          <p>
+            This tool measures JavaScript code execution and showing performance
+            comparison with exciting visual representation. Have fun!
+          </p>
+          <p>
+            <strong>How does it work?</strong>
+          </p>
+          <p>
+            It's using{" "}
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API"
+              target="_blank"
+            >
+              Web Workers
+            </a>{" "}
+            that runs code snippets in separate threads in the background.
+            Thanks to this browser API, the main thread is not blocked which
+            allows smooth animations. Each code snippet creates executable
+            function using{" "}
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/Function"
+              target="_blank"
+            >
+              Function constructor
+            </a>{" "}
+            which is then executed in loop for several seconds. The higher
+            numbers of execution, the better performance.
+          </p>
+          <p>
+            <strong>Why was it created?</strong>
+          </p>
+          <p>
+            Being software engineer at heart, I am often exploring how to write
+            better performing code. This inspired me to share this tool with the
+            community and hoping that many passionare developers will find it
+            useful as well.
+          </p>
+          <p>
+            <strong>Who created it?</strong>
+          </p>
+          <p>
+            Thanks for asking haha my name is{" "}
+            <a
+              href="https://www.linkedin.com/in/sanjinceleski/"
+              target="_blank"
+            >
+              Sanjin
+            </a>{" "}
+            and I would love to meet like minded people over a coffee if you
+            also live in Dubai 🇦🇪
+          </p>
+          <hr />
+          <p className="text-center">
+            <a
+              id="sanjin-celeski"
+              href="https://www.linkedin.com/in/sanjinceleski/"
+              target="_blank"
+            >
+              Sanjin Celeski
+            </a>
+          </p>
         </div>
       </div>
     </div>
